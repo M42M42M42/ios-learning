c828610045bd375f71289f63ae1e7aa2cc689623
xiao.li <m42m42m42@163.com>
2022-03-24 11:04:24 +0800

    [LiXiao] feat: first commit

---
+++ assignment1/assignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assignment1
+//
+//  Created by Xiao Li on 2022/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assignment1/assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by Xiao Li on 2022/3/24.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
